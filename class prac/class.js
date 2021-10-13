348f236a932567781feb1b56676f94f461ab4b04
VarzikaT <varzikatiwari08@gmail.com>
2021-10-13 09:30:20 +0530

    code

---
+++ class prac/class.js
@@ -0,0 +1,10 @@
+function fun1(){
+    alert(' button clicked');
+    var x=document.getElementById('heading');
+    x.innerHTML = "Hello Friends, you clicked it!";
+}
+
+function fun2(){
+    var x = document.getElementById('ab');
+    x.style.border="2px solid powderblue";
+}
