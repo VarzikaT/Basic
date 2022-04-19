c612c9352d1607e89e6a2e74eade7f61a5cb8836
VarzikaT <varzikatiwari08@gmail.com>
2022-04-19 15:13:39 +0530

    first files updated.

---
+++ Js-basics/JS.js
@@ -0,0 +1,33 @@
+/*
+Widely used by big companies like netflix, pypal to built their internal
+ We can use it build full-blown web o mobile apps& real time networking applications like
+ chats & video
+Javascript is run on javascript engines , js engines in firefox & chrome re SpiderMonkey 
+and v8 
+Node (Goggle's v8 JS engine) using which we can run code outside the browser also
+ECMA Script- Specification
+JavaScript- Programming Language
+As practtcise it is best to put the javascript script at the end of the body tags bcause:-
+1. Browser gets engaged in parsing the JS code if the code is at the strrt & 
+ultimately delivers the code late(bad user experienecE).
+2. 
+*/
+console.log('Hello World');
+/*
+Variable-used to store data temporarily 
+- cannot be a reserved keyword
+-meaningful & descriptive name
+-no special characters & numbers 
+-CamelCase notation & case sensitive
+-
+*/
+let na='mosh'; 
+console.log(na);
+
+//const keyword is used for the values whose valueswe doon't want to change 
+//otherwise use let keywoord  
+const interestRate=0.3;
+interestRate=1; // will give error
+console.log(interestRate);
+
+//
