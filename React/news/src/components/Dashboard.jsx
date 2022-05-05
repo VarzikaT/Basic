8b75c8e42d3436a56bcddae308e3ac3b18b799fd
VarzikaT <varzikatiwari08@gmail.com>
2022-05-05 16:47:01 +0530

    useEffect left

--- React/news/src/components/Dashboard.jsx
+++ React/news/src/components/Dashboard.jsx
@@ -3,28 +3,29 @@ import axios from "axios";
 import Card from './Card';
 import "./Dashboard.css"
 
+
 const Dashboard = () => {
 
   const [data, setData] = useState(1000);
 
   console.log(data);
 
-const options = {
-  method: 'GET',
-  url: 'https://bing-news-search1.p.rapidapi.com/news',
-  params: {safeSearch: 'Off', textFormat: 'Raw'},
-  headers: {
-    'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-    'X-RapidAPI-Key': 'd22eaa3c94msh7bf6560809ff00cp1526b6jsn6783f1e518dd'
-  }
-};
+  const options = {
+    method: 'GET',
+    url: 'https://bing-news-search1.p.rapidapi.com/news',
+    params: {safeSearch: 'Off', textFormat: 'Raw'},
+    headers: {
+      'X-BingApis-SDK': 'true',
+      'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+      'X-RapidAPI-Key': 'd22eaa3c94msh7bf6560809ff00cp1526b6jsn6783f1e518dd'
+    }
+  };
 
 
  useEffect(() => {
     axios.request(options).then(function (response) {
-      // console.log(response.data);
-      setData(response.data.value[0])
+      console.log(response.data);
+      setData(response.data.value)
     }).catch(function (error) {
       console.error(error);
     });  
@@ -41,7 +42,6 @@ const options = {
     <Card/>
         </div>
     <div>
-
       {data.description}
       Trending 
     </div>
