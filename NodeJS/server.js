a0be6b80895d2b562038664099b0f5d4ff2d21bf
Varzika Tiwari <varzikatiwari08@gmail.com>
2022-07-15 13:04:26 +0530

    save changes

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()  // this variable is required to call express function so that it works as a server
+app.use(logger)
+app.set('view engine', ejs) //compulsry code for view engine
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res, next)=>{
+
     console.log("Here")
     res.status(500).send("Hi")
+
     // res.send("Hi") //the send method sends the text within the braces (not preffered though)
     res.json("{message:Error")
 
     // res.download("script.js") this will create a pop up .....which will ultimately lead you to down,load  the file 
     // provided in the address
-    res.render(index)
+    res.render("index", {text:"welcome"})
 
 })  // here / is the path & request , respond are the variable used to create request and send response
+
+const userRouter =require('./routes/users')
+
+app.use('/users',userRouter)
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
 
 app.listen(3000) //3000  is the port where it will be hosted
---
+++ NodeJS/users.js
@@ -0,0 +1,41 @@
+const express =require('express')
+const router= express.Router()
+
+router.get("/",(req,res)=>{
+    res.send("User List")
+})
+
+router.get("/new",(req,res)=>{
+    res.send("User New Form")
+})
+// for creating a new user we need to create a post method
+router.post('/',(req,res)=>{
+    res.send('Create User')
+})
+// here /:id will work dynamically like for user  who have id 1,2 or anything
+// anything in the url with /1 /2 or /5678 
+
+// when applying https verbs on the same parameters we dont need to write get, push, delete separately 
+// as the express provides us with a single method to do all this functionality 
+router
+.route("/:id")
+.get((req,res)=>{
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req,res)=>{
+    res.send(`Update User With ID ${req.params.id}`)
+})
+.delete((req,res)=>{
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+const users=[{name:"Kyle"}, {name:"Sally"}]
+router.param("id",(req,res,next, id)=>{
+    console.log(id)
+    next() // next allows us to run the other functionality when a middleware like
+    // param starts executing w/o calling the ithers functions, so we give next to run the next func
+})
+
+//alwys try to put the dynamic routes at the last beacuse they can match
+// the existing call and provide you with wrong output
+module.exports= router //
