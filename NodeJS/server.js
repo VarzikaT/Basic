b7b8d00e3a2a0495095ccf46e06fd7869101c001
Varzika Tiwari <varzikatiwari08@gmail.com>
2022-07-20 11:01:09 +0530

    done eith the modifications

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()  // this variable is required to call express function so that it works as a server
+const express = require("express");
+const app = express(); 
+// this variable is required to call express function so that it works as a server
 // app.use(logger)
 
-app.use(express.static("public")) //this command serves all the files from the public folder as per requir
+app.use(express.static("public"));
+//this command serves all the files from the public folder as per require
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", 'ejs'); //compulsry code for view engine
 
-app.set('view engine', ejs) //compulsry code for view engine
+
 
 /*app.get('/',logger,(req,res, next)=>{
 
@@ -14,19 +19,25 @@ app.set('view engine', ejs) //compulsry code for view engine
     // res.send("Hi") //the send method sends the text within the braces (not preffered though)
     res.json("{message:Error")
 
-    // res.download("script.js") this will create a pop up .....which will ultimately lead you to down,load  the file 
+    // res.download("script.js") this will create a pop up .....which will ultimately lead you to download  the file 
     // provided in the address
     res.render("index", {text:"welcome"})
 
 })  // here / is the path & request , respond are the variable used to create request and send response
 */
-const userRouter =require('./routes/users')
 
-app.use('/users',userRouter)
+const userRouter = require("./users"); 
+
+app.use("/users", userRouter);
 
 // function logger(req, res, next){
 //     console.log(req.originalUrl)
 //     next()
 // }
 
-app.listen(3000) //3000  is the port where it will be hosted
+app.listen(5000); //5000  is the port where it will be hosted
+
+
+
+
+
--- NodeJS/users.js
+++ NodeJS/users.js
@@ -1,43 +1,73 @@
-const express =require('express')
-const router= express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.use(logger)
+router.get("/", (req, res) => {
+  console.log("Getting all users list");
+  // res.render("users/new",{firstName: "Test"})
 
-router.get("/",(req,res)=>{
-    res.render("users/new",{firstName: "Test"})
-})
+ 
+  let name = req.query.name;
+  let age = req.query.age;
+ 
+  console.log(name);
+  console.log(age);
+  var data = name + age;
 
-router.get("/new",(req,res)=>{
-    res.send("User New Form")
-})
+  res.send("User List" + data);
+});
+
+router.get("/new", (req, res) => {
+  res.send("User New Form");
+});
+
 // for creating a new user we need to create a post method
-router.post('/',(req,res)=>{
-    res.send('Create User') // displays the message create user when 
-})
+router.post("/", (req, res) => {
+
+    console.log(req.body);
+
+  res.send("Create User");
+
+
+  // displays the message create user when
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
+
+  // console.log(req.body.firstName)
+  // res.send("Hi")
+});
 // here /:id will work dynamically like for user  who have id 1,2 or anything
-// anything in the url with /1 /2 or /5678 
+// anything in the url with /1 /2 or /5678
 
-// when applying https verbs on the same parameters we dont need to write get, push, delete separately 
-// as the express provides us with a single method to do all this functionality 
+// when applying https verbs on the same parameters we dont need to write get, push, delete separately
+// as the express provides us with a single method to do all this functionality
 router
-.route("/:id")
-.get((req,res)=>{
-    res.send(`Get User With ID ${req.params.id}`)
-})
-.put((req,res)=>{
-    res.send(`Update User With ID ${req.params.id}`)
-})
-.delete((req,res)=>{
-    res.send(`Delete User With ID ${req.params.id}`)
-})
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`);
+  });
 
-const users=[{name:"Kyle"}, {name:"Sally"}]
-router.param("id",(req,res,next, id)=>{
-    console.log(id)
-    next() // next allows us to run the other functionality when a middleware like
-    // param starts executing w/o calling the ithers functions, so we give next to run the next func
-})
+const users = [
+    { name: "Kyle" }, 
+    { name: "Sally" }
+];
+router.param("id", (req, res, next, id) => {
+  console.log(id);
+  next(); // next allows us to run the other functionality when a middleware like
+  // param starts executing w/o calling the ithers functions, so we give next to run the next func
+});
 
 //alwys try to put the dynamic routes at the last beacuse they can match
 // the existing call and provide you with wrong output
-module.exports= router //
+module.exports = router; //
